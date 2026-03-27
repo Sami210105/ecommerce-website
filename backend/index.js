135bd81ec4f1434ded686f1512e97111707fa6d1
Samidha Dhawale <samidhadd21@gmail.com>
2026-03-27 23:54:19 +0530

    Modified index.js

--- backend/index.js
+++ backend/index.js
@@ -3,24 +3,32 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 require("dotenv").config();
 
+const Product = require("./models/Product"); // 👈 IMPORTANT
+
 const app = express();
+
+// middleware
 app.use(cors());
 app.use(express.json());
 
+// connect MongoDB
+mongoose
+  .connect("mongodb://localhost:27017/ecommerce")
+  .then(() => console.log("MongoDB connected 😌"))
+  .catch((err) => console.log(err));
+
+// test route
 app.get("/", (req, res) => {
   res.send("API is running");
 });
 
+// products route
 app.get("/api/products", async (req, res) => {
   const products = await Product.find();
   res.json(products);
 });
 
+// start server
 app.listen(5000, () => {
   console.log("Server is running on port 5000");
 });
-
-mongoose
-  .connect("mongodb://localhost:27017/ecommerce")
-  .then(() => console.log("MongoDB connected 😌"))
-  .catch((err) => console.log(err));
