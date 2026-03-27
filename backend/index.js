4ccddb3f6e660a21e1de38ce699d6b0604d9318f
Samidha Dhawale <samidhadd21@gmail.com>
2026-03-27 17:06:47 +0530

    Backend done!

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const cors = request("cors");
+const cors = require("cors");
 
 const app = express();
 app.use(cors());
@@ -12,13 +12,13 @@ app.get("/", (req, res) => {
 app.get("/api/products", (req, res) => {
   res.json([
     {
-      id: 1,
+      _id: 1,
       name: "Latte",
       price: 150,
       image:"https://i.pinimg.com/1200x/67/5a/ce/675ace2c99404fbab403c7762effada1.jpg",
     },
     {
-      id: 2,
+      _id: 2,
       name: "Frappe",
       price: 180,
       image:"https://i.pinimg.com/736x/ac/fd/9c/acfd9c5c59f1ce078f20bd059f93c984.jpg",
