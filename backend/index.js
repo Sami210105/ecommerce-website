7edeedaf6dd99efa16324bac634fe16d24ddfbeb
Samidha Dhawale <samidhadd21@gmail.com>
2026-03-27 23:52:25 +0530

    Connected mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = express();
 app.use(cors());
@@ -9,23 +11,16 @@ app.get("/", (req, res) => {
   res.send("API is running");
 });
 
-app.get("/api/products", (req, res) => {
-  res.json([
-    {
-      _id: 1,
-      name: "Latte",
-      price: 150,
-      image:"https://i.pinimg.com/1200x/67/5a/ce/675ace2c99404fbab403c7762effada1.jpg",
-    },
-    {
-      _id: 2,
-      name: "Frappe",
-      price: 180,
-      image:"https://i.pinimg.com/736x/ac/fd/9c/acfd9c5c59f1ce078f20bd059f93c984.jpg",
-    },
-  ]);
+app.get("/api/products", async (req, res) => {
+  const products = await Product.find();
+  res.json(products);
 });
 
 app.listen(5000, () => {
-    console.log("Server is running on port 5000");  
-})
+  console.log("Server is running on port 5000");
+});
+
+mongoose
+  .connect("mongodb://localhost:27017/ecommerce")
+  .then(() => console.log("MongoDB connected 😌"))
+  .catch((err) => console.log(err));
